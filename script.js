e6823af07b3ab1da61e421cbc52d8d195404a628
Livanenkova <arina909@yandex.ru>
2022-06-02 22:42:22 +0300

    add comeents

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+  // Список курсов
 let courses = [
   { name: "Courses in England", prices: [0, 100] }, 
   { name: "Courses in Germany", prices: [500, null] }, 
@@ -9,52 +10,69 @@ let courses = [
   { name: "Courses in France", prices: [null, null] },
 ];
 
+// Завожу переменные для html элементов
 const priceMin =  document.querySelector('.priceMin');
 const priceMax =  document.querySelector('.priceMax');
 const button =  document.querySelector('.button');
 const resultWrap =  document.querySelector('.result');
 
+// Вывожу на экран спсиок всех курсов 
+let coursesList = document.querySelector('.coursesList');
+courses.forEach(el => coursesList.innerHTML += `<div>${el.name} </div>`);
+
+// Создаю массив для хранения пользовательских данных
 const userValue = [];
 
+// Вешаю обработчик на кнопку чтобы при нажатии кнопки отслеживать ввод пользователя
 button.addEventListener('click', (event) => {
+  // Присваиваю переменным значение пользовательского ввода и заодно перевожу тип в число
   userValue[0] = Number(priceMin.value);
   userValue[1] = Number(priceMax.value);
+  // Присваиваю переменной вызов функции в которой происходит сортировка.
   const getresult = gerCourse(userValue);
+  // Передаю результат вызова функции сортировки в функцию вывода результата
   showResult(getresult);
 });
 
+// Функция выводящая результат на html страничку принимает массив и создает для каждого курса свой элемент
 function showResult(arr) {
   console.log('Ответ', courses);
   arr.forEach(el => resultWrap.innerHTML += `<div>${el.name} </div>`);
 }
 
-
+// Функция сортировки принимает массив и возвращает другой массив, отсортированный
+function gerCourse(arr) {
+  // Деструктурирую переменные для удобства сравнения
+  let [firstCellUser, SecondCellUser] = arr;
+  // Создаю массив для хранения результата вычисления
+  const result = [];
+  // Нужно прости по все курсам в массиве - уже цикл - создаю его
+  for( i = 0; i < courses.length; i ++) {
+    // Для удобства присваиваю переменным стоимость из курса
+    let firstCellCource = courses[i].prices[0];
+    let secondCellCource = courses[i].prices[1];
+    
+    // Для сравнения меняю первые hull на ноль, а второй на ифинити
+    firstCellUser === null ? firstCellUser = 0 : firstCellUser;
+    SecondCellUser === null ? SecondCellUser = Infinity : SecondCellUser;
+    firstCellCource === null  ? firstCellCource = 0 : firstCellCource;
+    secondCellCource === null ? secondCellCource = Infinity : firstCellCource;
+    // с помощью условия отбираю нужные курсы
+    if( firstCellUser >= firstCellCource || SecondCellUser >= secondCellCource ||  firstCellUser >= secondCellCource || SecondCellUser >= firstCellCource) {
+      // Результат пушу в новый массив созданный ранее
+      result.push(courses[i]);
+      }
+  }
+  // Возвращаю в место вызова массив с нужными курсами
+  return result
+}
 
 // Варианты цен (фильтры), которые ищет пользователь
 let requiredRange1 = [null, 200];
 let requiredRange2 = [100, 350];
 let requiredRange3 = [200, null];
 
-let coursesList = document.querySelector('.coursesList');
-courses.forEach(el => coursesList.innerHTML += `<div>${el.name} </div>`);
-
-
-function gerCourse(arr) {
-let [firstCellUser, SecondCellUser] = arr;
-const result = [];
-for( i = 0; i < courses.length; i ++) {
-let firstCellCource = courses[i].prices[0];
-let secondCellCource = courses[i].prices[1];
-
-firstCellUser === null ? firstCellUser = 0 : firstCellUser;
-SecondCellUser === null ? SecondCellUser = Infinity : SecondCellUser;
-firstCellCource === null  ? firstCellCource = 0 : firstCellCource;
-secondCellCource === null ? secondCellCource = Infinity : firstCellCource;
-
-if( firstCellUser >= firstCellCource || SecondCellUser >= secondCellCource ||  firstCellUser >= secondCellCource || SecondCellUser >= firstCellCource) {
-result.push(courses[i]);
-
-  }
-} 
-  return result
-}
+// Можно потестировать пользовательский ввод уже реализованный на сайте или использовать вызовы функции ниже)
+// gerCourse(requiredRange1);
+// gerCourse(requiredRange2);
+// gerCourse(requiredRange3);
